5d6d3f0ff9880a1bc60d5b67c4e91e8a95aa878b
silksil <silkreulen@gmail.com>
2018-12-17 14:23:32 +0100

    Create next.js

---
+++ clientAndServer/next.js
@@ -0,0 +1,4 @@
+Next.js is a React framework for building web applications. It does:
+- All of the tooling: webpack, code splitting etc.
+- It does server-side rendering: if you care about instant loading, pre-loading, seo etc., server-side rendering is important. 
+- it does the routing; there is no routing, there are pages. 
